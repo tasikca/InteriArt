93af87bfd0b7078ab42282f30b63d53f56adfa0d
Connor Tasik <123596825+tasikca@users.noreply.github.com>
2026-02-06 10:06:37 -0500

    Update main.py

--- main.py
+++ main.py
@@ -22,11 +22,12 @@ def update_plot(event=None):
     
     for path in c:
         cp, xMid = generatePath(A, b, path)
-        ax.plot(cp[:, 0], cp[:, 1], color=hexColor, ms=3, alpha=0.6)
-        #ax.plot(xMid[:, 0], xMid[:, 1], "o", ms=3, color=hexColor)
+        cpPlot = ax.plot(cp[:, 0], cp[:, 1], color=hexColor, marker="o", ms=3, alpha=0.6)
+        xMid = ax.plot(xMid[:, 0], xMid[:, 1], "o", ms=3, color=black)
 
     ax.set_title(f"{numFacets}-gon central path flower")
     ax.set_aspect('equal', adjustable='box')
+    ax.legend([cpPlot,xMid], ['accepted points on cp', 'bisec comparison points'])
     #ax.grid(True, linestyle='--', alpha=0.7)
     fig.tight_layout()
 
